6088689e5d566de9c6d84a23cec7d5aa2f9f98d5
Ilya Lubyanoy <subvert.dev@gmail.com>
2021-04-17 17:18:16 +0300

    Initial commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Subvert on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
