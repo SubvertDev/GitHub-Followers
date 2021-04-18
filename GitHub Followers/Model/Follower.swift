4d4a9032e35de77ddb41e02213b9fad2c54c26c2
Ilya Lubyanoy <subvert.dev@gmail.com>
2021-04-18 20:32:49 +0300

    Add user and follower models

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Subvert on 18.04.2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHub Followers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GitHub Followers
+//
+//  Created by Subvert on 18.04.2021.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
