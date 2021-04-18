29a34c6a2f169f7c9ce471f709f07367307c6484
Ilya Lubyanoy <subvert.dev@gmail.com>
2021-04-18 20:42:36 +0300

    Fix swipe back nav bar title bug

--- GitHub Followers/Screens/FollowersListVC.swift
+++ GitHub Followers/Screens/FollowersListVC.swift
@@ -14,8 +14,11 @@ class FollowersListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
 }
