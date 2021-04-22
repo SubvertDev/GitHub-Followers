7c238472d8c61c866304f1fd493cf7d832271fb8
Ilya Lubyanoy <subvert.dev@gmail.com>
2021-04-22 22:55:09 +0300

    Add loading new page animation

--- GitHub Followers/Screens/FollowersListVC.swift
+++ GitHub Followers/Screens/FollowersListVC.swift
@@ -48,8 +48,10 @@ class FollowersListVC: UIViewController {
     }
     
     func getFollowers(username: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoading()
             
             switch result {
             case .success(let followers):
