89bcb4a4bf4169adb85d9ddab4d74db584843172
Ilya Lubyanoy <subvert.dev@gmail.com>
2021-04-28 22:09:11 +0300

    Add logic to add favorite button

--- GitHub Followers/Screens/FavoritesVC.swift
+++ GitHub Followers/Screens/FavoritesVC.swift
@@ -12,6 +12,16 @@ class FavoritesVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
+        
     }
-
+    
 }
